4225444a7f08429479a9671b97d97c71b4395708
Amit Biswas <amit.142biswas@gmail.com>
2024-09-25 21:18:37 +0530

    image upload api

---
+++ app/api/image-upload/route.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { v2 as cloudinary } from 'cloudinary';
+import { auth } from '@clerk/nextjs/server';
+
+// Configuration
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+});
+
+interface CloudinaryUploadResult {
+    public_id: string;
+    [key: string]: any
+}
+
+export async function POST (request: NextRequest) {
+    const {userId} = auth()
+
+    if (userId) {
+        return NextResponse.json({error: "Unauthorized"}, {status: 401})
+    }
+}
